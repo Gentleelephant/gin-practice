0fd8227d634f3cdd1dc8da19b224eae50e57cefb
zhangpeng <1132960613@qq.com>
2022-06-10 14:20:51 +0800

    feat(): ✅ 添加邮件发送测试

---
+++ src/mail/mail_test.go
@@ -0,0 +1,33 @@
+package mail
+
+import (
+	"log"
+	"net/smtp"
+	"testing"
+
+	"github.com/jordan-wright/email"
+)
+
+func TestMail(t *testing.T) {
+
+	e := email.NewEmail()
+
+	// 设置发送方邮箱
+	e.From = "1665400978@qq.com"
+
+	// 设置接收方邮箱
+	e.To = []string{"1132960613@qq.com"}
+
+	// 设置主题
+	e.Subject = "这是主题"
+
+	// 设置内容
+	e.Text = []byte("www.topgoer.com是个不错的go语言中文文档")
+
+	// 设置服务器配置并发送
+	err := e.Send("smtp.qq.com:25", smtp.PlainAuth("", "1665400978@qq.com", "srwrbfpblphtbcac", "smtp.qq.com"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
